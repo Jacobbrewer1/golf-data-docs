0aea9b6704137e4701102e2f9d201b94c7329a9a
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-08-21 16:53:21 +0100

    removing the repo after cloning

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run --rm \
-  -v "${PWD}"/golf-data:/local openapitools/openapi-generator-cli generate \
-  -i /local/pkg/codegen/apis/api/routes.yaml \
+  -v "${PWD}":/local openapitools/openapi-generator-cli generate \
+  -i /local/golf-data/pkg/codegen/apis/api/routes.yaml \
   -g markdown \
   -o /local/docs
