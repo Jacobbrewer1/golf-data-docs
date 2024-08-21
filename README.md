f68f70a58f83f7bdd2a364eb9735026e3a56a7ae
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-08-21 16:57:00 +0100

    Updating the repo format

--- README.md
+++
@@ -1,9 +0,0 @@
-# Golf Data Docs
-
-Please find all the documentation for the Golf Data API here.
-
-The host and base path for the API is:
-
-```URL
-https://api.bthree.uk/golf/v1
-```
--- gen-docs.sh
+++ gen-docs.sh
@@ -4,4 +4,4 @@ docker run --rm \
   -v "${PWD}":/local openapitools/openapi-generator-cli generate \
   -i /local/golf-data/pkg/codegen/apis/api/routes.yaml \
   -g markdown \
-  -o /local/docs
+  -o /local
