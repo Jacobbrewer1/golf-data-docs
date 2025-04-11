cc8e9e6a228b19505ff4171e278fb38a153222b3
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-11 13:28:16 +0100

    Update gen-docs.sh

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+rm -rf README.md
+rm -rf Apis
+rm -rf Models
+
 docker run --rm \
   -v "${PWD}":/local openapitools/openapi-generator-cli generate \
   -i /local/golf-data/pkg/apis/specs/api/routes.yaml \
